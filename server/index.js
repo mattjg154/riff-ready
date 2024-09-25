382c82f6f52f3549fa5c6153b7ce3eba96328e35
Matthew Goulding <mxg183@student.bham.ac.uk>
2024-09-25 13:32:56 +0100

    implemented auth callback endpoint

--- server/index.js
+++ server/index.js
@@ -1,8 +1,45 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
 const app = express();
+
 
 app.use("/", (req,res)=>{
     res.send("Server is running");
 });
+
+app.get("/auth/login", (req,res)=>{
+    var code = req.query.code;
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: "http://localhost:3000/auth/callback",
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+      'Content-Type' : 'application/x-www-form-urlencoded'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.redirect('/')
+    }
+  });
+});
+
+app.get("/auth/callback", (req,res)=>{
+
+})
 
 app.listen(5000, console.log("Server is on port 5000"));
